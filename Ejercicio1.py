c93b1f782cc092daac15395216e9480ac74d9364
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-11-08 08:55:26 +0100

    Ejercicio 2 y 1
    Subo ejercicio 2 y corrección del 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,8 +1,7 @@
 '''Escribir una función a la que se le pase una cadena <nombre>
  y muestre por pantalla el saludo ¡hola <nombre>!.'''
-nombre = input("Escribe tu nombre: ")
 def bienvenida(nombre):
-    """"Funcion que muestra por pantalla el saludo ¡hola <nombre>!"""
+    """Funcion que muestra por pantalla el saludo ¡hola <nombre>!"""
     print('¡Hola', nombre, end="!")
     return
-bienvenida(nombre)
+bienvenida("Iñaki")
---
+++ Ejercicio2.py
@@ -0,0 +1,15 @@
+'''Escribir una función que reciba un número entero positivo y devuelva su factorial.
+ Realiza el ejercicio mediante bucles interactivos y mediante una función recursiva.'''
+def factorial_bucle(num):
+    """Recibe un número entero positivo y devuelva su factorial"""
+    res = 1
+    for i in range(1, num + 1):
+        res = res * i
+    return res
+def factorial_recursivo(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial_recursivo(num - 1)
+print(factorial_bucle(5))
+print(factorial_recursivo(5))
