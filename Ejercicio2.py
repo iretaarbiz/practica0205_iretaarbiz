419fb3a28a1a9cb8ef11d994314b28db24526849
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-11-08 08:56:35 +0100

    Comentario función

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,12 +1,13 @@
 '''Escribir una función que reciba un número entero positivo y devuelva su factorial.
  Realiza el ejercicio mediante bucles interactivos y mediante una función recursiva.'''
 def factorial_bucle(num):
-    """Recibe un número entero positivo y devuelva su factorial"""
+    """Recibe un número entero positivo y devuelva su factorial mediante bucle for"""
     res = 1
     for i in range(1, num + 1):
         res = res * i
     return res
 def factorial_recursivo(num):
+    """Recibe un número entero positivo y devuelva su factorial mediante recursividad"""
     if num == 0:
         return 1
     else:
